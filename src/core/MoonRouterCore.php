54ad1914654720089dd3f5902d77e4ccf010cfe1
amir <baghshahyamyr@gmail.com>
2023-09-06 14:43:15 +0330

    Update MoonRouterCore.php

--- src/core/MoonRouterCore.php
+++ src/core/MoonRouterCore.php
@@ -2,151 +2,98 @@
 
 namespace src\core;
 
-use Closure;
 use Exception;
 
-abstract class MoonRouterCore
+class MoonRouterCore
 {
+    private array $redirectedRoutes = [];
+    private array $routes = [];
+    private array $params = [];
 
-    private array $redirected_routes = array();
-    private array $routes = array();
-    private array $params = array();
-
-
-    /**
-     * @param string $path
-     * @param Closure|array $callback
-     * @param string $method
-     * @return void
-     */
-    protected function addRoute(string $path, Closure|array $callback, string $method): void
+    public function addRoute(string $path, $callback, string $method): void
     {
         $this->routes[$method][$path] = $callback;
-
     }
 
-
-    /**
-     * @param string $from
-     * @param string $to
-     * @return void
-     */
-    protected function addRedirectRoute(string $from, string $to): void
+    public function addRedirectRoute(string $from, string $to): void
     {
-        $this->redirected_routes[$from] = $to;
+        $this->redirectedRoutes[$from] = $to;
     }
 
-
-    /**
-     * @param string $path
-     * @return void
-     */
-    private function checkRedirectRoute(string $path): void
+    private function isRedirectRoute(string $path): bool
     {
-        if (array_key_exists($path, $this->redirected_routes)) {
-            MoonRouterHttp::redirectRoute($this->redirected_routes[$path]);
-        }
+        return array_key_exists($path, $this->redirectedRoutes);
     }
 
-
-    /**
-     * @return bool|string]
-     * @throws Exception
-     */
-    private function resolveActionFromPath(): bool|Closure|array
+    private function resolveActionFromPath(string $url): mixed
     {
-        try {
-            $url = MoonRouterHttp::getUrlFromRequest();
-
-            $this->checkRedirectRoute($url);
-            foreach ($this->routes as $method => $key) {
-                foreach ($key as $path => $callback) {
-                    $regex = '~^(' . preg_replace('~\{([a-zA-Z0-9]+)\}~', '([^/]+)', $path) . ')$~';
-                    preg_match($regex, $url, $match_url);
-                    if ($match_url) {
-                        $this->resolveparams($match_url, $path);
-                        return $this->routes[MoonRouterHttp::getHttpMethod()][$path] ?? false;
-                    }
+        foreach ($this->routes as $method => $routes) {
+            foreach ($routes as $path => $callback) {
+                $pattern = '~^(' . str_replace('{', '([^/]+)', preg_quote($path, '~')) . ')$~';
+                if (preg_match($pattern, $url, $matches)) {
+                    $this->resolveParams($matches, $path);
+                    return $callback;
                 }
             }
-            return false;
-        } catch (Exception $e) {
-            throw  new  Exception($e->getMessage());
+        }
+        return false;
+    }
+
+    private function resolveParams(array $matches, string $path): void
+    {
+        preg_match_all('~\{([a-zA-Z0-9]+)\}~', $path, $paramNames);
+        $paramNames = $paramNames[1];
+        array_shift($matches); // Remove the full match
+
+        foreach ($paramNames as $index => $paramName) {
+            $this->params[$paramName] = $matches[$index];
         }
     }
 
-
-    /**
-     * @param array $match_url
-     * @param string $path
-     * @return void
-     */
-    private function resolveParams(array $match_url, string $path): void
+    private function resolveAction($action): mixed
     {
-        try {
-            preg_match_all('~\{([a-zA-z0-9]+)\}~', $path, $paramNames);
-            $paramValues = array_slice($match_url, 2);
-            unset($paramNames[0]);
-
-            foreach ($paramNames[1] as $index => $key) {
-                $this->params[$key] = $paramValues[$index];
-            }
-        } catch (Exception $e) {
-            throw  new  Exception($e->getMessage());
+        if (is_callable($action)) {
+            return $action();
         }
+
+        if (is_array($action) && count($action) === 2 && is_string($action[0]) && is_string($action[1])) {
+            [$className, $methodName] = $action;
+
+            if (class_exists($className)) {
+                $classInstance = new $className();
+                if (method_exists($classInstance, $methodName)) {
+                    return $classInstance->$methodName();
+                } else {
+                    throw new Exception("Method '$methodName' not found in class '$className'");
+                }
+            } else {
+                throw new Exception("Class '$className' not found");
+            }
+        }
+
+        throw new Exception("Invalid action provided");
     }
 
-
-    /**
-     * @param array|Closure $action
-     * @return mixed
-     * @throws Exception
-     */
-    private function resolveAction(array|Closure $action): mixed
+    public function run(): void
     {
-        try {
+        $requestedUrl = MoonRouterHttp::getUrlFromRequest();
 
-            $__request = new MoonRouterHttp($_GET, $_POST, $_FILES, $_SERVER, $this->params);
-
-
-            if ($action instanceof Closure) {
-                return call_user_func($action, $__request);
-            }
-
-            if (class_exists($action[0])) {
-                if (method_exists($action[0], $action[1])) {
-                    $action[0] = new $action[0];
-                    return call_user_func($action, $__request);
-                } else throw new Exception("method {{$action[1]}}  not found in class {{$action[0]}} ");
-            } else throw new Exception("class {{$action[0]}} not found");
-
-        } catch (Exception $e) {
-            throw  new  Exception($e->getMessage());
+        if ($this->isRedirectRoute($requestedUrl)) {
+            MoonRouterHttp::redirectRoute($this->redirectedRoutes[$requestedUrl]);
+            return;
         }
 
-    }
+        $action = $this->resolveActionFromPath($requestedUrl);
 
-
-    /**
-     * @return int|void
-     * @throws Exception
-     */
-    protected function run()
-    {
-        try {
-            $result = $this->resolveActionFromPath();
-
-            if ($result == null) {
-                return MoonRouterHttp::sendHttpCode(404);
-            } elseif (is_bool($result) && !$result) {
-                return MoonRouterHttp::sendHttpCode(405);
-            }
-
-            $this->resolveAction($result);
-
-        } catch (Exception $e) {
-            throw  new  Exception($e->getMessage());
+        if ($action === false) {
+            MoonRouterHttp::sendHttpCode(404);
+            return;
         }
 
+        try {
+            $this->resolveAction($action);
+        } catch (Exception $e) {
+            throw new Exception($e->getMessage());
+        }
     }
 }
